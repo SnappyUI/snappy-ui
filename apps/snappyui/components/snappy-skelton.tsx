003062aaf1430795511f53397559a0024525892b
Ajay patel <patel.ajay745@gmail.com>
2025-04-01 08:26:13 -0400

    fix: animation in  skeleton component (#21)

--- apps/snappyui/components/snappy-skelton.tsx
+++ apps/snappyui/components/snappy-skelton.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { cn } from "./utils/cn";
+import { cn } from "@/lib/cn";
 
 type SkeletonType = "text" | "circular" | "rectangular" | "rounded";
 type AnimationType = "pulse" | false;
