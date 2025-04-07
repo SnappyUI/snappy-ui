950fbadc06535dcca47ae8b7cbf3c2a0d119c00a
Ayush_Bhagat <103896902+AyushBhagat151105@users.noreply.github.com>
2025-04-07 22:40:42 +0530

    feat: add accordion component
    * fix: update tsconfig to include snappy-tab component path
    
    * chore: update code structure for better readability and maintainability and add accordion
    
    * feat: replace GitHub icon with custom SVG for improved performance

--- apps/snappyui/app/(home)/page.tsx
+++ apps/snappyui/app/(home)/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { Github as GitHubIcon } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -48,7 +47,18 @@ export default function HomePage() {
             onClick={(e) => { e.preventDefault(); }}
             className="p-4 bg-gray-200 cursor-not-allowed dark:bg-gray-800 rounded-lg hover:bg-gray-300 dark:hover:bg-gray-700 transition-colors flex flex-row items-center gap-2"
           >
-            <GitHubIcon />
+            <svg
+              role="img"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              width={24}
+              height={24}
+            >
+              <title>GitHub</title>
+              <path d="M12 0C5.373 0 0 5.373 0 12c0 5.302 3.438 9.8 8.207 11.385.6.11.793-.26.793-.577v-2.234c-3.338.724-4.043-1.61-4.043-1.61-.546-1.387-1.333-1.756-1.333-1.756-1.09-.745.083-.729.083-.729 1.205.085 1.84 1.238 1.84 1.238 1.07 1.835 2.805 1.305 3.49.997.108-.775.42-1.305.762-1.605-2.665-.305-5.466-1.332-5.466-5.93 0-1.31.467-2.38 1.235-3.22-.123-.303-.535-1.525.116-3.176 0 0 1.008-.322 3.3 1.23.956-.266 1.98-.398 3-.403 1.02.005 2.045.137 3 .403 2.29-1.552 3.297-1.23 3.297-1.23.653 1.65.24 2.873.118 3.176.77.84 1.233 1.91 1.233 3.22 0 4.61-2.805 5.622-5.475 5.922.43.372.81 1.103.81 2.222v3.293c0 .32.19.694.8.576C20.565 21.795 24 17.3 24 12c0-6.627-5.373-12-12-12z" />
+            </svg>
+
             {" "}
             Give us Star
           </Link>
