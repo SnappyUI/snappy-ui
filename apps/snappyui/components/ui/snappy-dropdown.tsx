c5e760a157d65e659923866cbebdd309f4937a45
Ajay Patel <patel.ajay745@gmail.com>
2025-04-09 15:05:26 -0400

    fix: solve dropdown problem

--- apps/snappyui/components/ui/snappy-dropdown.tsx
+++ apps/snappyui/components/ui/snappy-dropdown.tsx
@@ -50,38 +50,46 @@ DropdownMenuSubTrigger.displayName = DropdownMenuPrimitive.SubTrigger.displayNam
 
 function DropdownMenuSubContent({ ref, className, ...props }: React.ComponentPropsWithoutRef<typeof DropdownMenuPrimitive.SubContent> & { ref?: React.RefObject<React.ElementRef<typeof DropdownMenuPrimitive.SubContent> | null> }) {
   return (
+
     <DropdownMenuPrimitive.SubContent
       ref={ref}
       className={cn(
-        "z-50 min-w-[8rem] overflow-hidden rounded-md border bg-popover p-1 text-popover-foreground shadow-lg data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-form-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 origin-[--radix-dropdown-menu-content-transform-origin]",
+        "fixed z-50 max-h-[var(--radix-dropdown-menu-content-available-height)] min-w-[8rem] overflow-y-auto overflow-x-hidden rounded-md border bg-popover text-popover-foreground shadow-md",
+        "data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 origin-top-left",
+        "-left-10",
         className,
       )}
+      sideOffset={8}
+      alignOffset={0}
       {...props}
-    >
-    </DropdownMenuPrimitive.SubContent>
+    />
   );
 }
 
 DropdownMenuSubContent.displayName
-    = DropdownMenuPrimitive.SubContent.displayName;
+  = DropdownMenuPrimitive.SubContent.displayName;
 
-function DropdownMenuContent({ ref, className, sideOffset = 4, size = "default", ...props }: React.ComponentPropsWithRef<typeof DropdownMenuPrimitive.Content> & {
+function DropdownMenuContent({ ref, className, sideOffset = 4, size = "default", side = "bottom", ...props }: React.ComponentPropsWithRef<typeof DropdownMenuPrimitive.Content> & {
   size?: "default" | "large" | "compact";
 } & { ref?: React.RefObject<React.ElementRef<typeof DropdownMenuPrimitive.Content> | null> }) {
   return (
     <DropdownMenuPrimitive.Portal>
       <DropdownMenuPrimitive.Content
         ref={ref}
+        side={side}
         sideOffset={sideOffset}
         className={cn(
-          "z-50 max-h-[var(--radix-dropdown-menu-content-available-height)] min-w-[8rem] overflow-y-auto overflow-x-hidden rounded-md border bg-popover text-popover-foreground shadow-md",
-          "data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 origin-[--radix-dropdown-menu-content-transform-origin]",
+          "fixed z-50 max-h-[var(--radix-dropdown-menu-content-available-height)] min-w-[8rem] overflow-y-auto overflow-x-hidden rounded-md border bg-popover text-popover-foreground shadow-md",
+          "data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
+          "origin-top- left",
+          "mt-1 left-1/2 -translate-x-1/2",
 
           size === "default" && "p-2",
           size === "large" && "p-4",
           size === "compact" && "p-0.5",
           className,
         )}
+        align="center"
 
         {...props}
       />
