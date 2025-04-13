c61c6ba306d7a23c29506cf689f85c124724b3af
Jay Kadlag <jaykadlag14@gmail.com>
2025-04-13 13:18:12 +0530

    feat(home): implement hero section

--- apps/snappyui/app/(home)/page.tsx
+++ apps/snappyui/app/(home)/page.tsx
@@ -6,22 +6,15 @@ import Footer from "@/components/footer";
 import Hero from "@/components/hero";
 import Team from "@/components/team";
 
-// import Image from "next/image";
-// import Link from "next/link";
-
-// import { ColourfulText } from "@/components/ui/snappy-colorful-text";
-
 export default function HomePage() {
   return (
-    <main className="flex flex-1 flex-col items-center justify-center w-screen h-screen relative overflow-hidden bg-white dark:bg-black">
-      {/* Background Image */}
-
-      <main className="max-w-[1400px] mx-auto">
+    <main className="flex flex-col items-center w-screen relative overflow-hidden bg-white dark:bg-black">
+      <div className="max-w-[1440px] w-full mx-auto">
         <Hero />
         <Features />
         <Components />
         <Team />
-      </main>
+      </div>
       <Footer />
     </main>
   );
