2062397618c3792ca70cac5caa7f7319db3ce7d6
Aditya Raj <aditya0503sfs@gmail.com>
2025-04-16 18:24:39 +0530

    fix: the build error

---
+++ apps/snappyui/components/scratch.tsx
@@ -0,0 +1,25 @@
+"use client";
+import React from "react";
+
+import ScratchToReveal from "@/components/ui/snappy-scratch";
+
+export default function Scratch() {
+  const handleComplete = () => {
+    // Do Something
+  };
+  return (
+    <div>
+      <ScratchToReveal
+        width={250}
+        height={250}
+        minScratchPercentage={70}
+        className="flex items-center justify-center overflow-hidden rounded-2xl border-2 bg-gray-100"
+        onComplete={handleComplete}
+      >
+        <p className="text-9xl">
+          🍑
+        </p>
+      </ScratchToReveal>
+    </div>
+  );
+}
