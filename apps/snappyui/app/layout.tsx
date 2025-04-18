e3acbf94f82bc8f92aed09191d2d10625298fa2b
Ajay Patel <patel.ajay745@gmail.com>
2025-04-18 18:02:31 -0400

    feat: add google analitcs

--- apps/snappyui/app/layout.tsx
+++ apps/snappyui/app/layout.tsx
@@ -4,6 +4,7 @@ import type { ReactNode } from "react";
 
 import { RootProvider } from "fumadocs-ui/provider";
 import { Inter } from "next/font/google";
+import Script from "next/script";
 
 import { NavbarProvider } from "@/components/nav-mobile";
 import { Navbar } from "@/components/navbar";
@@ -15,6 +16,20 @@ const inter = Inter({
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" className={inter.className} suppressHydrationWarning>
+      <head>
+
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-PNW5GZRGVP"></Script>
+        <Script>
+          {
+            ` window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-PNW5GZRGVP');`
+          }
+
+        </Script>
+      </head>
       <body className="bg-background font-sans relative ">
         <RootProvider theme={{
           enableSystem: true,
